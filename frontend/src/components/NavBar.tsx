c9c431ab188c9f6396ac53d8212e8c442dbbd19a
Alexey Kaledkin <hakon1@mail.ru>
2023-11-15 19:52:52 +0300

    добавил поиск

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,36 +1,21 @@
-/* eslint-disable react/jsx-closing-tag-location */
 import { useTranslation } from 'react-i18next';
-import { useContext, useEffect, useState } from 'react';
-import { Link, useNavigate, useSearchParams } from 'react-router-dom';
-import { Dropdown, AutoComplete } from 'antd';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Dropdown } from 'antd';
 import type { MenuProps } from 'antd';
 import cn from 'classnames';
-import { toLower } from 'lodash';
-import {
-  Navbar, Container, Button, Nav,
-} from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import ProfileButton from './ProfileButton';
-import fetchImage from '../utilities/fetchImage';
-import { useAppSelector, useAppDispatch } from '../utilities/hooks';
-import { selectors, searchUpdate } from '../slices/marketSlice';
+import { useAppSelector } from '../utilities/hooks';
+import { selectors } from '../slices/marketSlice';
 import { MobileContext } from './Context';
 import routes from '../routes';
 import type { Item } from '../types/Item';
-
-type Data = { name: string, image: string }[];
+import Search from './Search';
 
 const NavBar = () => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
   const isMobile = useContext(MobileContext);
-  const dispatch = useAppDispatch();
-
-  const [search, setSearch] = useState<string>();
-  const [data, setData] = useState<Data>([]);
-
-  const [urlParams] = useSearchParams();
-  const urlPage = Number(urlParams.get('page'));
-  const urlSearch = urlParams.get('q');
 
   const itemsMarket: Item[] = useAppSelector(selectors.selectAll);
 
@@ -75,71 +60,6 @@ const NavBar = () => {
     },
   ];
 
-  console.log(search);
-  console.log(urlSearch);
-
-  const searchHandler = (searchedValue: string | undefined) => {
-    console.log(searchedValue);
-    if (!searchedValue) {
-      dispatch(searchUpdate(null));
-    } else {
-      const result = itemsMarket.filter(({ name, composition }) => {
-        const query = toLower(searchedValue);
-        return toLower(name).includes(query) || toLower(composition).includes(query);
-      });
-      if (result.length) {
-        dispatch(searchUpdate(result));
-        navigate(`/?q=${searchedValue}&page=${urlPage}`);
-        console.log(searchedValue);
-        setSearch(searchedValue);
-      } else {
-        dispatch(searchUpdate(null));
-        navigate(`/search?q=${searchedValue}`);
-      }
-      const target = document.body;
-      target.parentElement?.focus();
-    }
-  };
-
-  const onSearch = async (q: string) => {
-    const images: string[] = [];
-    const result = itemsMarket.filter(({ name, composition, image }) => {
-      const query = toLower(q);
-      images.push(image);
-      return toLower(name).includes(query) || toLower(composition).includes(query);
-    });
-    const fetchedData = await Promise.all(result.map(async ({ name, image }) => {
-      const fetchedImage = await fetchImage(image);
-      return { name, image: fetchedImage };
-    }));
-    setData(fetchedData);
-  };
-
-  const options = (d: Data) => d.map(({ name, image }) => ({
-    label: <Button
-      onClick={() => {
-        searchHandler(name);
-      }}
-      className="ant-select-item ant-select-item-option ant-select-item-option-active"
-      title={name}
-    >
-      <div className="ant-select-item-option-content d-flex align-items-center gap-2">
-        <img className="col-md-2" alt={name} src={image} />
-        <span className="fs-6">{name}</span>
-      </div>
-    </Button>,
-  }));
-
-  const handleChange = (value: string) => setSearch(value);
-
-  useEffect(() => {
-    if (urlSearch && itemsMarket.length) {
-      searchHandler(urlSearch);
-    } else {
-      setSearch(undefined);
-    }
-  }, [itemsMarket, urlSearch]);
-
   return (
     <Navbar expand={isMobile ? 'xxl' : true}>
       <Container>
@@ -161,18 +81,10 @@ const NavBar = () => {
             </Dropdown>
           </Nav>
         </Navbar.Collapse>
-        <Navbar.Collapse className="justify-content-end col-md-4 me-3">
-          <AutoComplete
-            value={search}
-            className="d-flex col-md-10"
-            placeholder={t('navBar.search')}
-            allowClear
-            options={options(data)}
-            onSearch={onSearch}
-            status="warning"
-          />
+        <Navbar.Collapse className="justify-content-start col-md-3">
+          <Search items={itemsMarket} />
         </Navbar.Collapse>
-        {!isMobile && <ProfileButton className="col-md-2" />}
+        {!isMobile && <ProfileButton className="d-flex justify-content-end" />}
       </Container>
     </Navbar>
   );
